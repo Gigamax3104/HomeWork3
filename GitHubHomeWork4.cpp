5ca9b8faf47af1aade52b9b236372efcfc25e6bf
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-09-18 19:40:05 +0900

    プロジェクト ファイルを追加します。

---
+++ GitHubHomeWork4.cpp
@@ -0,0 +1,44 @@
+#include	<iostream>
+#include	<list>
+#include	<cstring>
+using	namespace std;
+
+char* strrmv_digits(char* s);
+
+int main() {
+	int size = 0;
+	char Code[18];
+	char* Code2;
+
+	do{
+		size = 0;
+
+		cout << "•¶Žš“ü—Í >" << flush;
+		cin >> Code;
+
+		for (int i = 0; i < sizeof Code; i++) {
+			if (Code[i] == '\0') {
+				break;
+			}
+			size++;
+		}
+
+	} while (size < 8);
+
+	Code2 = strrmv_digits(Code);
+
+	for (int i = 0; i < size;i++) {
+		cout << Code2 + i << flush;
+	}
+}
+
+char* strrmv_digits(char* s) {
+
+	for (int i = 0; *(s + i) != '\0'; i++) {
+		if (*(s + i) >= '0' && *(s + i) <= '9') {
+			*(s + i) = '\0';
+		}
+	}
+
+	return s;
+}
